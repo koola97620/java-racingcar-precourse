78ea4311385c9ac478effe45c955b6fcb1bccdbe
koola97620 <koola97620@nate.com>
2021-10-13 15:52:28 +0900

    refactor: RandomNumber 삭제

--- src/main/java/racinggame/domain/RandomMovable.java
+++ src/main/java/racinggame/domain/RandomMovable.java
@@ -5,14 +5,11 @@ import nextstep.utils.Randoms;
 public class RandomMovable implements MovingStrategy {
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
+    private static final int MOVING_CONDITION_NUMBER = 4;
 
     @Override
     public boolean movable() {
-        RandomNumber randomNumber = createRandomNumber();
-        return randomNumber.isMovable();
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER) >= MOVING_CONDITION_NUMBER;
     }
 
-    private RandomNumber createRandomNumber() {
-        return RandomNumber.of(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
-    }
 }
